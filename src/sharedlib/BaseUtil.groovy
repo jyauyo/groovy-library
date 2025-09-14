00552b7dc7befe70b5b5bd40979a20cb6bbc33b0
jyauyo <jyauyo@outlook.com.pe>
2025-09-13 23:25:39 -0500

    Update BaseUtil.groovy

--- src/sharedlib/BaseUtil.groovy
+++ src/sharedlib/BaseUtil.groovy
@@ -1,28 +1,30 @@
 package sharelib
 
-protected script;
-protected script;
-protected String type;
-
 abstract class BaseUtil {
+  protected script
+  protected String type
+  protected String openJdkJava = "open-jdk-21"
+  protected def currentCredentialsId
+  protected String sonarqubeurl
   
-    protected String openJdkJava = "open-jdk-21"
-    protected def currentCredentialsId
-
-    protected BaseUtil() {}
-    
-    protected BaseUtil(script, String type = '') {
-      this.script = script
-      this.type =  type
-    
-      def remoteConfigs =  this.script.scm.getUserRemoteConfigs()
-      for (Object remoteConfig : remoteConfigs) {
-        this.currentCredentialsId = remoteConfig.getCredentialsId()
-        printMessage("currentCredentialsId for ${remoteConfig}: ${currentCredentialsId}")
-      }
-    }
+  protected BaseUtil() {}
   
-    protected void printMessage(String message) {
-      this.script.steps.echo "[DEVOPS] ${message}"
+  protected BaseUtil(script, String type = '') {
+    this.script = script
+    this.type =  type
+  
+    def remoteConfigs =  this.script.scm.getUserRemoteConfigs()
+    for (Object remoteConfig : remoteConfigs) {
+      this.currentCredentialsId = remoteConfig.getCredentialsId()
+      printMessage("currentCredentialsId for ${remoteConfig}: ${currentCredentialsId}")
     }
   }
+  
+  public void prepare() {
+    this.sonarqubeurl = "";
+  }
+  
+  protected void printMessage(String message) {
+    this.script.steps.echo "[DEVOPS] ${message}"
+  }
+}
