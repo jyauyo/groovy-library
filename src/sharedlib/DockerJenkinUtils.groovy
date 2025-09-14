89c649d3ccae62df2fe15d5e3b12edca22d75117
jyauyo <jyauyo@outlook.com.pe>
2025-09-14 00:14:37 -0500

    Update DockerJenkinUtils.groovy

--- src/sharedlib/DockerJenkinUtils.groovy
+++ src/sharedlib/DockerJenkinUtils.groovy
@@ -16,9 +16,11 @@ class DockerJenkinUtils extends BaseUtil {
 
     printMessage("***** Creating Dockerfile")
     
-     this.script.steps.writeFile file: 'Dockerfilea', text:"""
+    def jarName = script.steps.sh(script: "ls target/*.jar | head -1", returnStdout: true).trim()
+    
+     this.script.steps.writeFile file: 'Dockerfile', text:"""
      FROM eclipse-temurin:21-jdk-alpine
-     ADD ${params.jarName} /app/service.jar
+     ADD ${jarName} /app/service.jar
      WORKDIR /app
      ENTRYPOINT ["java", "-jar", "/app/service.jar"]
      """
@@ -26,6 +28,20 @@ class DockerJenkinUtils extends BaseUtil {
     def docker_registry_environment_ = "${script.env.DOCKER_REGISTRY_ENVIRONMENT}"
     def docker_registry_complete = "${script.env.DOCKER_REGISTRY}"
     printMessage("***** Docker Registry Final: ${docker_registry_complete}");
+
+
+
+    def dockerfile = 'Dockerfile'
+   def customImage = docker.build("${docker_registry_complete}/${params.projectName}:${params.version}", "-f ${dockerfile} .")
+
+   withCredentials([usernamePassword(credentialsId: "${env.DOCKER_CREDENTIALS_ID}", usernameVariable: 'dockerHubUser', passwordVariable: 'dockerHubPassword')]){
+
+       script.echo  "${script.env.dockerHubPassword} | login --username ${script.env.dockerHubUser} --password-stdin  ${script.env.DOCKER_URL}"                   
+       //sh "docker push ${env.DOCKER_REGISTRY}${env.DOCKER_REGISTRY_ENVIRONMENT}/app-microservice:${APP_VERSION}"
+   }
+   printMessage("***** Publishing to Docker Registry: ${APP_VERSION}")
+   customImage.push()
+    
     //echo gitAuthorName()//other groovy
   }
 }
