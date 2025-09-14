dd5b4f27fb7af85a2f054252c697f9ef71f0d12d
jyauyo <jyauyo@outlook.com.pe>
2025-09-13 22:53:34 -0500

    Update DockerJenkinUtils.groovy

--- src/sharedlib/DockerJenkinUtils.groovy
+++ src/sharedlib/DockerJenkinUtils.groovy
@@ -2,6 +2,10 @@ package sharedlib
 
 class DockerJenkinUtils {
 
+  public DockerJenkinUtils(script, String type = ''){
+    echo '**** DockerJenkinUtils ****'
+  }
+
   public build(Map config) {
     sh 'echo Hi From DevOps Team'
     echo "${config.projectName}"
