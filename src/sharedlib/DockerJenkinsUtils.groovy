dba40ee7a7c34fb527436365607413f821cc672a
jyauyo <jyauyo@outlook.com.pe>
2025-09-17 14:15:20 -0500

    Update DockerJenkinsUtils.groovy

--- src/sharedlib/DockerJenkinsUtils.groovy
+++ src/sharedlib/DockerJenkinsUtils.groovy
@@ -7,7 +7,7 @@ class DockerJenkinsUtils extends BaseUtil {
   }
 
   public build() {
-    script.echo("***** Creating Dockerfile - DockerJenkinsUtils")
+    this.script.steps.echo("***** Creating Dockerfile - DockerJenkinsUtils")
     
     //sh 'echo Hi From DevOps Team'
     //printMessage("${params.projectName}")
