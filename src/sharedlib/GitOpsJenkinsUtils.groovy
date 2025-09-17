ba4fa7f83fc9acc0144d6e2ee7183956d02e843a
jyauyo <jyauyo@outlook.com.pe>
2025-09-17 14:43:14 -0500

    Update GitOpsJenkinsUtils.groovy

--- src/sharedlib/GitOpsJenkinsUtils.groovy
+++ src/sharedlib/GitOpsJenkinsUtils.groovy
@@ -26,6 +26,20 @@ class GitOpsJenkinsUtils extends BaseUtil {
     printMessage("***** Sync With ArgoCd")
 
     script.withCredentials([script.usernamePassword(credentialsId: "${script.env.ARGOCD_CREDENTIALS_ID}", usernameVariable: 'ARGOCD_USERNAME', passwordVariable: 'ARGOCD_PASSWORD')]){
+
+      /*
+      argocd app create capturador-plataformaunica \
+      --repo http://gitlab.insi.sunat.peru/gestionsaldos/recaudacionms2-tributaria-gsc-capturador-plataformaunica-backend.git \
+      --revision 2023-001 \
+      --path k8s/desarrollo/capturador-plataformaunica \
+      --dest-server https://172.26.58.21:8443 \
+      --dest-namespace gestionsaldos \
+      --project gestion-saldos \
+      --label paseNro=PASEXXX \
+      --label version=1.1.0-1 \
+      --grpc-web;
+      */
+      
       script.sh "argocd login ${script.env.ARGOCD_HOST} --username ${script.env.ARGOCD_USERNAME} --password ${script.env.ARGOCD_PASSWORD} --insecure"
       script.sh "argocd app set ${projectName} --sync-policy none --grpc-web;"
       script.sh "argocd app set ${projectName} --revision ${BRANCH} --grpc-web;"
