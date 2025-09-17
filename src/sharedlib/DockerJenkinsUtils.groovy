efe6d6416f3d3e545b17c566a6c554a929fc147e
jyauyo <jyauyo@outlook.com.pe>
2025-09-17 11:55:42 -0500

    Update DockerJenkinsUtils.groovy

--- src/sharedlib/DockerJenkinsUtils.groovy
+++ src/sharedlib/DockerJenkinsUtils.groovy
@@ -30,7 +30,7 @@ class DockerJenkinsUtils extends BaseUtil {
    def customImage = script.docker.build("${docker_registry_complete}/${projectName}:${version}", "-f ${dockerfile} .")
 
    script.withCredentials([script.usernamePassword(credentialsId: "${script.env.DOCKER_CREDENTIALS_ID}", usernameVariable: 'dockerHubUser', passwordVariable: 'dockerHubPassword')]){
-     script.echo "${script.env.dockerHubPassword} | login --username ${script.env.dockerHubUser} --password-stdin  ${script.env.DOCKER_URL}"
+     script.steps.echo "${script.env.dockerHubPassword} | login --username ${script.env.dockerHubUser} --password-stdin  ${script.env.DOCKER_URL}"
      printMessage("***** Publishing to Docker Registry: ${version}")
      customImage.push()
    }
