5c091f474961a0a0c617d016c4b3421982a26a28
jyauyo <jyauyo@outlook.com.pe>
2025-09-17 14:34:16 -0500

    Update DockerJenkinsUtils.groovy

--- src/sharedlib/DockerJenkinsUtils.groovy
+++ src/sharedlib/DockerJenkinsUtils.groovy
@@ -6,12 +6,12 @@ class DockerJenkinsUtils extends BaseUtil {
     super(script, type)
   }
 
-  public build() {
+  public build(Map params) {
     printMessage("***** Creating Dockerfile - DockerJenkinsUtils")
-    printMessage("***** Publishing : ${version}")
+
     //sh 'echo Hi From DevOps Team'
-    //printMessage("${params.projectName}")
-    //printMessage("${params.version}")   
+    printMessage("${params.projectName}")
+    printMessage("${params.version}")   
     
     def jarName = script.sh(script: "ls target/*.jar | head -1", returnStdout: true).trim()
     
@@ -27,11 +27,11 @@ class DockerJenkinsUtils extends BaseUtil {
     printMessage("***** Docker Registry Final: ${docker_registry_complete}");
 
     def dockerfile = 'Dockerfile'
-   def customImage = script.docker.build("${docker_registry_complete}/${projectName}:${version}", "-f ${dockerfile} .")
+   def customImage = script.docker.build("${docker_registry_complete}/${params.projectName}:${params.version}", "-f ${dockerfile} .")
 
    script.withCredentials([script.usernamePassword(credentialsId: "${script.env.DOCKER_CREDENTIALS_ID}", usernameVariable: 'dockerHubUser', passwordVariable: 'dockerHubPassword')]){
      this.script.echo "${script.env.dockerHubPassword} | login --username ${script.env.dockerHubUser} --password-stdin  ${script.env.DOCKER_URL}"
-     printMessage("***** Publishing to Docker Registry: ${version}")
+     printMessage("***** Publishing to Docker Registry: ${params.version}")
      customImage.push()
    }
   }
