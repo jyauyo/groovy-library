792e37b5d76138d46955aef51b19ba0b7ee9712c
jyauyo <jyauyo@outlook.com.pe>
2025-09-19 23:09:05 -0500

    Update defaultAgent.groovy

--- vars/defaultAgent.groovy
+++ vars/defaultAgent.groovy
@@ -1,7 +1,8 @@
 def call() {
-      return [
-          label: 'agent-maven-argocd-jdk21'            
-          docker: image 'jyauyor/maven-argocd-jdk21:1.0.4'            
-          args: '-v /var/run/docker.sock:/var/run/docker.sock -v $HOME/.m2:/var/maven/.m2:z -e MAVEN_CONFIG=/var/maven/.m2 -e MAVEN_OPTS=-Duser.home=/var/maven'
-      ]
+      return {
+        docker {
+            image 'jyauyor/maven-argocd-jdk21:1.0.4'
+            args '-v /var/run/docker.sock:/var/run/docker.sock -v $HOME/.m2:/var/maven/.m2:z -e MAVEN_CONFIG=/var/maven/.m2 -e MAVEN_OPTS=-Duser.home=/var/maven'
+        }
+    }
 }
